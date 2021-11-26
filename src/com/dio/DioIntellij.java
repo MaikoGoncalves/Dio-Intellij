f9595fff10c02ebb2ae63c4583c6ef8ace1b745a
MaikoGoncalves <maikosgoncalves@gmail.com>
2021-11-25 23:08:55 -0300

    Novo commit

--- src/com/dio/DioIntellij.java
+++ src/com/dio/DioIntellij.java
@@ -2,6 +2,12 @@ package com.dio;
 
 public class DioIntellij {
     public static void main(String[] args) {
-        
+        Gato gato = new Gato();
+
+        int a = 10;
+        int b = 3;
+        System.out.println("Soma de A + B = " + (a + b));
+        System.out.println(gato);
+
     }
 }
